b5bba4971bf3fd3b4705da3f6ae89a992f30ef4c
Sea <seafr@users.noreply.github.com>
2017-08-19 22:10:53 +0400

    Run migrations. Seed database.

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,17 @@
 ActiveRecord::Schema.define(version: 20170817201739) do
 
   create_table "destinations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "image"
+    t.string   "description"
+    t.integer  "tag_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string   "title"
+    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
